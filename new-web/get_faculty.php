f691b3e006b4409a379b431b206e7c40ac7a4de6
Vaishnavidasyam <vaishnavi.41469@gmail.com>
2025-09-26 17:38:55 +0530

    Delete new-web directory

--- new-web/get_faculty.php
+++
@@ -1,8 +0,0 @@
-<?php
-$subject = $_GET['subject'];
-// Fetch faculty based on subject
-$stmt = $pdo->prepare("SELECT faculty_name FROM subfac WHERE subject = ?");
-$stmt->execute([$subject]);
-$faculty = $stmt->fetchColumn();
-echo json_encode(['faculty' => $faculty]);
-?>
--- new-web/staff1.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-if (!isset($_SESSION['faculty_name'])) {
-    header("Location: faclogin.html");
-    exit();
-}
-$facultyName = $_SESSION['faculty_name'];
-include("staff1.html");
-?>
